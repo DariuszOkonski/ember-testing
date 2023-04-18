611623ac8c28d9e9914e6baf78a276a769938df7
Dariusz Okoński <darek200180@gmail.com>
2023-04-18 15:56:00 +0200

    testing components - pretty-color

--- app/components/pretty-color.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-import Component from '@glimmer/component';
-
-export default class PrettyColorComponent extends Component {
-  get style() {
-    return `color: ${this.args.name}`;
-  }
-}
