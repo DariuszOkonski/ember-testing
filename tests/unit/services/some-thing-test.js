bd0a1d61b17311d0a012b3e6f940e5f0bb9f4295
Dariusz Okoński <darek200180@gmail.com>
2023-04-25 19:49:37 +0200

    repeat basics tests

--- tests/unit/services/some-thing-test.js
+++ tests/unit/services/some-thing-test.js
@@ -2,51 +2,68 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-testing/tests/helpers';
 
-module('unit | service | some-thing', function (hooks) {
+module('unit | services | some-thing', function (hooks) {
   setupTest(hooks);
 
+  test('should be able to change foo state', function (assert) {
+    const testSomeThing = this.owner.lookup('service:some-thing');
+
+    testSomeThing.foo = 'baz';
+
+    assert.strictEqual(testSomeThing.foo, 'baz');
+  });
+
+  test('should be able to chang count state', function (assert) {
+    const testSomeThing = this.owner.lookup('service:some-thing');
+
+    testSomeThing.count = 1;
+
+    assert.strictEqual(testSomeThing.count, 1);
+  });
+
   test('should correctly concat foo', function (assert) {
-    const someThing = this.owner.lookup('service:some-thing');
+    const testSomeThing = this.owner.lookup('service:some-thing');
 
-    someThing.foo = 'baz';
+    testSomeThing.foo = 'baz';
 
-    assert.strictEqual(someThing.computedFoo, 'computed baz');
+    assert.strictEqual(testSomeThing.computedFoo, 'computed baz');
   });
 
   test('should update foo on updateFoo method', function (assert) {
-    const someThing = this.owner.lookup('service:some-thing');
+    const testSomeThing = this.owner.lookup('service:some-thing');
 
-    someThing.updateFoo();
+    testSomeThing.updateFoo();
 
-    assert.strictEqual(someThing.foo, 'baz');
+    assert.strictEqual(testSomeThing.foo, 'baz');
   });
 
   test('should return incremented count on calc', function (assert) {
-    const someThing = this.owner.lookup('service:some-thing');
+    const testSomeThing = this.owner.lookup('service:some-thing');
 
-    assert.strictEqual(someThing.calc(), 'count: 1');
-    assert.strictEqual(someThing.calc(), 'count: 2');
+    testSomeThing.count = 1;
+
+    assert.strictEqual(testSomeThing.calc(), 'count: 2');
+    assert.strictEqual(testSomeThing.calc(), 'count: 3');
   });
 
-  test('should return number 8 when provided number 4', function (assert) {
-    const someThing = this.owner.lookup('service:some-thing');
+  test('should return number 8 when provided number 4 to complicatedMethos', function (assert) {
+    const testSomeThing = this.owner.lookup('service:some-thing');
 
-    const result = someThing.complicatedMethod(4);
+    const result = testSomeThing.complicatedMethod(4);
 
     assert.strictEqual(result, 8);
   });
 
   test('countAnswer should return result of complicatedMethod', function (assert) {
-    const someThing = this.owner.lookup('service:some-thing');
+    const testSomeThing = this.owner.lookup('service:some-thing');
+    const originalComplicatedMethod = testSomeThing.complicatedMethod;
 
-    const originalComplicatedMethod = someThing.complicatedMethod;
-
-    someThing.complicatedMethod = function () {
+    testSomeThing.complicatedMethod = function () {
       return 4;
     };
 
-    assert.strictEqual(someThing.countAnswer(2), 'Answer: 4');
+    assert.strictEqual(testSomeThing.countAnswer(2), 'Answer: 4');
 
-    someThing.complicatedMethod = originalComplicatedMethod;
+    testSomeThing.complicatedMethod = originalComplicatedMethod;
   });
 });
